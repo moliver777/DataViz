e84dfe2eb46c62ba83609fe54fc44fed759839fa
Mark Oliver <moliver777@googlemail.com>
2014-09-09 17:30:40 +0100

    First commit

---
+++ dataviz/dataviz.js
@@ -0,0 +1,136 @@
+var DataViz = {
+	INSTANCES: {},
+	
+	// build data visualisation
+	// returns array [success, error-message]
+	build: function(viz, options) {
+		try {
+			if (DataViz.INSTANCES[options["name"]]) throw "Visualisation already initialised with name: "+options["name"];
+			switch (viz) {
+				case "ExactaDonut" : DataViz.INSTANCES[options["name"]] = new ExactaDonut(options); break;
+				case "DoubleDonut" : DataViz.INSTANCES[options["name"]] = new DoubleDonut(options); break;
+				case "OddsTracker" : DataViz.INSTANCES[options["name"]] = new OddsTracker(options); break;
+				case "DistributionTracker" : DataViz.INSTANCES[options["name"]] = new DistributionTracker(options); break;
+				case "FormComparison" : DataViz.INSTANCES[options["name"]] = new FormComparison(options); break;
+				case "ProfitAndLoss" : DataViz.INSTANCES[options["name"]] = new ProfitAndLoss(options); break;
+			}
+			return [true,null];
+		} catch(e) {
+			return [false,e];
+		}
+	},
+	
+	saddleCloth: function(type, runner) {
+		var code = null;
+		switch (type) {
+			case 'GREYHOUND' : 
+				switch (parseInt(runner)) {
+					case 0: code="#F0EFEE";break;
+					case 1: code='#CE1126';break;
+					case 2: code='#0072C6';break;
+					case 3: code="#FFFFFF";break;
+					case 4: code='#00B760';break;
+					case 5: code='#000000';break;
+					case 6: code='#F9D616';break;
+					case 7: code='#23AD17';break;
+					case 8: code='#F9D300';break;
+					default: code='#00B2AA';break;
+				}
+			break;
+			case 'HARNESS' : 
+				switch (parseInt(runner)) {
+					case 0: code="#F0EFEE";break;
+					case 1: code="#FF0000";break;
+					case 2: code='#3333FF';break;
+					case 3: code='#FFFFFF';break;
+					case 4: code='#009F04';break;
+					case 5: code='#000000';break;
+					case 6: code='#E8DA00';break;
+					case 7: code='#D106C1';break;
+					case 8: code='#CCCCCC';break;
+					case 9: code='#6600CC';break;
+					case 10: code='#F30021';break;
+					case 11: code='#000000';break;
+					case 12: code='#000000';break;
+					default: code='#00B2AA';break;
+				}
+			break;
+			default : 
+				switch (parseInt(runner)) {
+					case 0: code="#F0EFEE";break;
+					case 1: code='#CE1126';break;
+					case 2: code='#FFFFFF';break;
+					case 3: code='#0072C6';break;
+					case 4: code='#F9D616';break;
+					case 5: code='#00B760';break;
+					case 6: code='#000000';break;
+					case 7: code='#F74902';break;
+					case 8: code='#F2ADB2';break;
+					case 9: code='#00B2AA';break;
+					case 10: code='#9E4FA5';break;
+					case 11: code='#D1CEC6';break;
+					case 12: code='#7FBA00';break;
+					case 13: code='#593D2B';break;
+					case 14: code='#7A2638';break;
+					case 15: code='#C4B796';break;
+					case 16: code='#5960A8';break;
+					case 17: code='#2F2FFF';break;
+					case 18: code='#007F01';break;
+					case 19: code='#5986CC';break;
+					default: code='#870539';break;
+				}
+			break;
+		}
+		return code;
+	},
+	
+	saddleClothText: function(type, runner) {
+		var code = null;
+		switch (type) {
+			case 'GREYHOUND' : 
+				switch (parseInt(runner)) {
+					case 1: code='#FFFFFF';break;
+					case 2: code='#FFFFFF';break;
+					case 5: code='#FF0000';break;
+					case 8: code='#FFFFFF';break;
+					default: code='#000000';break;
+				}
+			break;
+			case 'HARNESS' : 
+				switch (parseInt(runner)) {
+					case 1: code='#FFFFFF';break;
+					case 2: code='#FFFFFF';break;
+					case 3: code='#F10101';break;
+					case 4: code='#000000';break;
+					case 5: code='#FFFFFF';break;
+					case 6: code='#010101';break;
+					case 7: code='#000000';break;
+					case 8: code='#000000';break;
+					case 9: code='#FFFFFF';break;
+					case 10: code='#FFFFFF';break;
+					case 11: code='#FFFFFF';break;
+					case 12: code='#FFFFFF';break;
+					default: code='#222222';break;
+				}
+			break;
+			default : 
+				switch (parseInt(runner)) {
+					case 1: code='#FFFFFF';break;
+					case 3: code='#FFFFFF';break;
+					case 5: code='#FFFFFF';break;
+					case 6: code='#F9D616';break;
+					case 10: code='#FFFFFF';break;
+					case 11: code='#CE1126';break;
+					case 14: code='#F9D616';break;
+					case 16: code='#F74902';break;
+					case 17: code='#FFFFFF';break;
+					case 18: code='#FFFFFF';break;
+					case 19: code='#FFFFFF';break;
+					case 20: code='#FFFFFF';break;
+					default: code='#000000';break;
+				}
+			break;
+		}
+		return code;
+	}
+}
---
+++ dataviz/viz/double-donut.js
@@ -0,0 +1,281 @@
+// ------------------------ \\
+// ----- DOUBLE DONUT ----- \\
+// ------------------------ \\
+var DoubleDonut = function DoubleDonut(options) {
+	// TODO throw errors if required options are invalid
+	this._init(options);
+}
+
+// Rebuild exacta donut with full animation (runner focus change)
+// Returns status object including odds values for focussed runner
+DoubleDonut.prototype.change = function(bettingInterestNumber) {
+	try {
+		if (this.runner != String(bettingInterestNumber) && bettingInterestNumber <= this.numRunners1 && bettingInterestNumber > 0) {
+			this.runner = String(bettingInterestNumber);
+			this._fullAnimate();
+		}
+		return this._status();
+	} catch(e) {
+		return this._status(e);
+	}
+}
+
+// Rebuild exacta donut with partial animation (odds update)
+// Returns status object including odds values for focussed runner
+DoubleDonut.prototype.update = function(oddsJson) {
+	try {
+		this.lastUpdated = new Date();
+		this.data = oddsJson;
+		this._partialAnimate();
+		return this._status();
+	} catch(e) {
+		return this._status(e);
+	}
+}
+
+// ----------------------------- \\
+// ----- PRIVATE FUNCTIONS ----- \\
+// ----------------------------- \\
+DoubleDonut.prototype._init = function(options) {
+	this.name = options["name"];
+	this.container = "#"+options["container"];
+	this.height = Math.min($(this.container).height(),$(this.container).width());
+	this.width = this.height;
+	this.rOuter = this.height/2;
+	this.rInner = this.rOuter*0.4;
+	
+	this.data = options["data"];
+	this.total = 0;
+	this.runner = '1';
+	this.numRunners1 = parseInt(options["numRunners1"]);
+	this.numRunners2 = parseInt(options["numRunners2"]);
+	this.type = options["type"].toUpperCase();
+	this.originDataset = new Array();
+	this.targetDataset = new Array();
+	this.lastUpdated = new Date();
+	
+	this._build();
+}
+
+DoubleDonut.prototype._teardown = function() {
+	this.container = null;
+	this.height = null;
+	this.width = null;
+	this.rOuter = null;
+	this.rInner = null;
+	this.data = null;
+	this.total = null;
+	this.runner = null;
+	this.numRunners1 = null;
+	this.numRunners2 = null;
+	this.type = null;
+	this.originDataset = null;
+	this.targetDataset = null;
+	this.lastUpdated = null;
+}
+
+// build initial state of viz
+DoubleDonut.prototype._build = function() {
+	var self = this;
+	$(this.container).empty();
+	this.originDataset = this._dataset(); // format initial dataset
+	
+	// create svg canvas
+	var exa = d3.select("#"+$(this.container).attr('id'))
+		.append("svg:svg")
+		.attr("width",this.width)
+		.attr("height",this.height)
+		.attr("id","doubleSvg"+this.name);
+	
+	// create focussed runner indicator circle and text
+	exa.append("svg:circle")
+		.attr("class","dblCircle")
+		.attr("cx",this.rInner)
+		.attr("cy",this.rInner)
+		.attr("r",this.rInner+0.5)
+		.attr("transform","translate("+(this.rOuter-this.rInner)+","+(this.rOuter-this.rInner)+")")
+		.attr("fill",function(){return DataViz.saddleCloth(self.type,self.runner)});
+	exa.append("svg:text")
+		.attr("class","dblText")
+		.attr("dx",function(){return self.width/2})
+		.attr("dy",function(){return self.height/2})
+		.attr("font-size",function(){return self.rInner+"px"})
+		.attr("fill",function(d,i){return DataViz.saddleClothText(self.type,self.runner)})
+		.attr("opacity",1)
+		.attr("style","text-anchor:middle;dominant-baseline:central;")
+	  .text(this.runner);
+	
+	// create paths
+	exa.selectAll("g.dblArc")
+		.data(this._arcs(this.originDataset,this.originDataset))
+		.enter().append("svg:g")
+		.attr("class","dblArc")
+		.attr("transform","translate("+this.rOuter+","+this.rOuter+")")
+		.append("svg:path")
+		.attr("fill",function(d,i){return DataViz.saddleCloth(self.type,i)})
+		.attr("d",d3.svg.arc())
+		.attr("id",function(d,i){return i});
+}
+
+// trigger full animation sequence (fracture -> shuffle -> reunite)
+DoubleDonut.prototype._fullAnimate = function() {
+	var self = this;
+	this.targetDataset = this._dataset(); // format new dataset
+	
+	// animate focussed runner elements
+	focussedRunnerAnimate();
+	
+	// select all paths and push new positions to fracture->shuffle->reunite animations
+	d3.selectAll("g.dblArc > path")
+		.data(this._arcs(this.originDataset,this.targetDataset))
+		.each(fracture);
+	this.originDataset = this.targetDataset.slice(0); // set origin same as target (animation complete)
+	
+	// fracture animation definition (full #1)
+	function fracture(d,i) {
+		d3.select(this)
+			.transition().duration(500)
+			.attrTween("d",tween(radiusMod()))
+			.each("end",shuffle);
+	}
+
+	// shuffle animation definition (full #2)
+	function shuffle(d,i) {
+		d3.select(this)
+			.transition().duration(500)
+			.attrTween("d",tween({
+				startAngle: d.next.startAngle,
+				endAngle: d.next.endAngle
+			}))
+			.each("end",reunite);
+	}
+
+	// reunite animation definition (full #3)
+	function reunite(d,i) {
+		d3.select(this)
+			.transition().duration(500)
+			.attrTween("d",tween({
+				innerRadius: self.rInner,
+				outerRadius: self.rOuter
+			}));
+	}
+	
+	// random modification of fracture radii
+	function radiusMod() {
+		switch (Math.round(Math.random()*3)) {
+			case 0: var innerMod=0; var outerMod=(self.rOuter-self.rInner)*.25; break;
+			case 1: var innerMod=(self.rOuter-self.rInner)*.25; var outerMod=(self.rOuter-self.rInner)*.5; break;
+			case 2: var innerMod=(self.rOuter-self.rInner)*.5; var outerMod=(self.rOuter-self.rInner)*.75; break;
+			case 3: var innerMod=(self.rOuter-self.rInner)*.75; var outerMod=self.rOuter-self.rInner; break;
+		}
+		return {
+			innerRadius: innerMod+self.rInner,
+			outerRadius: outerMod+self.rInner
+		}
+	}
+	
+	// animation tweening
+	function tween(b) {
+		return function(a) {
+			var i = d3.interpolate(a,b);
+			for (var key in b) a[key] = b[key];
+			return function(t) {
+				return d3.svg.arc()(i(t));
+			}
+		}
+	}
+	
+	// focussed-runner animation definition
+	function focussedRunnerAnimate() {
+		// morph circle fill
+		d3.selectAll("circle.dblCircle").transition().duration(1500)
+			.attr("fill",function(){return DataViz.saddleCloth(self.type,self.runner)});
+		// animate text: fade out -> change content and fill -> fade in
+		d3.selectAll("text.dblText").transition().duration(700)
+			.attr("opacity",0)
+			.each("end",function() {
+				d3.select(this).transition().duration(100)
+					.attr("fill",function(d,i){return DataViz.saddleClothText(self.type,self.runner)})
+					.text(self.runner)
+					.each("end",function() {
+						d3.select(this).transition().duration(700)
+							.attr("opacity",1);
+					});
+			});
+	}
+}
+
+// trigger partial animation sequence (reposition)
+DoubleDonut.prototype._partialAnimate = function() {
+	var self = this;
+	this.targetDataset = this._dataset(); // format new dataset
+	
+	// select all paths and push new positions reposition animation
+	d3.selectAll("g.dblArc > path")
+		.data(this._arcs(this.originDataset,this.targetDataset))
+		.each(reposition);
+	this.originDataset = this.targetDataset.slice(0); // set origin same as target (animation complete)
+	
+	// reposition animation definition (partial)
+	function reposition(d,i) {
+		d3.select(this)
+			.transition().duration(500)
+			.attrTween("d",tween({
+				startAngle: d.next.startAngle,
+				endAngle: d.next.endAngle
+			}));
+	}
+	
+	// animation tweening
+	function tween(b) {
+		return function(a) {
+			var i = d3.interpolate(a,b);
+			for (var key in b) a[key] = b[key];
+			return function(t) {
+				return d3.svg.arc()(i(t));
+			}
+		}
+	}
+}
+
+// build dataset
+DoubleDonut.prototype._dataset = function() {
+	var self = this;
+	var dataset = new Array();
+	// build target dataset for animation of viz
+	this.total = 0;
+	if (this.data && (this.data instanceof Object)) {
+		for (i=1;i<=this.numRunners2;i++) {
+			dataset[i] = parseFloat(this.data[this.runner+','+i]);
+			if (isNaN(dataset[i])) dataset[i] = 0; // rescue NaN values
+			this.total += dataset[i];
+		}
+	}
+	dataset[0] = (this.total > 0) ? 0 : 1; // rescue for empty odds (give runner0 100% of distribution)
+	$.each(dataset, function(i,v) { if (v>0) dataset[i] = 1/v }); // invert dataset (1/v for distribution)
+	return dataset
+}
+
+// layout
+DoubleDonut.prototype._arcs = function(x,y) {
+	var currentArcs = d3.layout.pie()(x);
+	var newArcs = d3.layout.pie()(y);
+	var i = -1;
+	while (++i <= this.numRunners2) {
+		arc = currentArcs[i];
+		arc.innerRadius = this.rInner;
+		arc.outerRadius = this.rOuter;
+		arc.next = newArcs[i];
+	}
+	return currentArcs;
+}
+
+// build status object of viz
+DoubleDonut.prototype._status = function(e) {
+	return {
+		data: this.data,
+		error: e,
+		lastUpdated: this.lastUpdated,
+		status: (e ? false : true)
+	}
+}
